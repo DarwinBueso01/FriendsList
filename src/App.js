32e1ab51809aa7015624f1732c02cb543a9ae03a
Darwin Bueso <darwingaldamez5@gmail.com>
2020-06-10 19:26:33 -0400

    Pushing Friends List

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
 import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
+import FriendCard from "./components/FriendCard";
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
   state = {
     friends
-  };
+  }
 
   removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
+    const friends = this.state.friends.filter(friend => friend.id !== id)
+    this.setState({ friends })
+  }
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
+
   render() {
     return (
       <Wrapper>
         <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
+        {this.state.friends.map(friends => (
           <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
+            removeFriend = {this.removeFriend}
+            id = {friends.id}
+            key = {friends.id}
+            name = {friends.name}
+            image= {friends.image}
+            occupation = {friends.occupation}
+            location = {friends.location}
           />
         ))}
       </Wrapper>
